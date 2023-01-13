6eea0daca3b4a4ac2918ca9e96897229c77fcc4e
JonasM <jonas.moewes02@gmail.com>
2023-01-13 18:52:27 +0100

    Einlesen verbessert, man kann jetzt alle Dateien einlesen, welche ihre Klasse in der letzten Spalte haben

--- src/Einlesen/Einlesen.java
+++ src/Einlesen/Einlesen.java
@@ -2,20 +2,20 @@ package Einlesen;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Einlesen {
-
-    public static double[][] einlesen(String s,int[] attributes,String[] clazzes, int clazzId) throws FileNotFoundException {
+    public static double[][] einlesen(String s,int attributes,String[] clazzes, int clazzId) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(s));
-        boolean isNumeric = clazzes[0].chars().allMatch(Character::isDigit);
+        Map<String,Integer> clazzesMap = new HashMap<>();
+        for (int i = 0;i< clazzes.length;i++){
+            clazzesMap.put(clazzes[i],i);
+        }
         int muster = 0;
-        double[] maximas = new double[attributes.length];
+        double[] maximas = new double[attributes];
 
         while (scanner.hasNext()){
-            for(int i = 0;i<attributes.length+1;i++) {
+            for(int i = 0;i<attributes+1;i++) {
                 double value = Double.parseDouble(scanner.next());
                 if (i == clazzId) {
                     continue;
@@ -25,14 +25,19 @@ public class Einlesen {
             muster++;
         }
 
-        double[][] inputs = new double[muster][attributes.length+clazzes.length];
+        double[][] inputs = new double[muster][attributes+clazzes.length];
         scanner = new Scanner(new File(s));
         for(int m = 0;m<muster;m++){
-            for(int i = 0;i<attributes.length+1;i++){
-                double value = Double.parseDouble(scanner.next());
-                if (i >= clazzId || i<clazzId+clazzes.length){
+            for(int i = 0;i<attributes+1;i++){
+                if (i == clazzId){
+                    int clazz = clazzesMap.get(scanner.next());
+                    for(int n = 0;n<clazzes.length;n++){
+                        if(n == clazz) inputs[m][i+n] = 1;
+                        else inputs[m][n] = 0;
+                    }
                     continue;
                 }
+                double value = Double.parseDouble(scanner.next());
                 inputs[m][i] = value;
             }
         }
--- src/NeuralNetworkTest/TestClass.java
+++ src/NeuralNetworkTest/TestClass.java
@@ -1,80 +1,91 @@
 package NeuralNetworkTest;
 
-import NeuralNetwork.ActivationFunction.ActivationFunction;
-import NeuralNetwork.ActivationFunction.Sigmoid;
-import NeuralNetwork.NeuralNetwork;
-import NeuralNetwork.InputNeuron;
-import NeuralNetwork.WorkingNeuron;
+import NeuralNetwork.*;
+import NeuralNetwork.ActivationFunction.*;
+
 import Einlesen.Einlesen;
 
 import java.io.FileNotFoundException;
 
 public class TestClass {
-    private static double learnrate = 0.0001;
-    private static ActivationFunction activationFunction = ActivationFunction.ActivationBool;
+    private static final double learnrate = 0.0001;
+    private static final ActivationFunction activationFunction = ActivationFunction.ActivationBool;
+    private static final int features = 4;
+    public static final int clazzId = 4;
+    private static final int outputs = 3;
 
     public static void main(String[] args) throws FileNotFoundException {
+        double[][] inputs = Einlesen.einlesen("src/Einlesen/Iris.txt",features,new String[]{"1","2","3"},clazzId);
         NeuralNetwork neuralNetwork = new NeuralNetwork();
-        InputNeuron in1 = neuralNetwork.createInputNeuron();
-        InputNeuron in2 = neuralNetwork.createInputNeuron();
-//        InputNeuron in3 = neuralNetwork.createInputNeuron();
-//        InputNeuron in4 = neuralNetwork.createInputNeuron();
+        InputNeuron[] inputNeurons = new InputNeuron[features];
+        WorkingNeuron[] outputNeurons = new WorkingNeuron[outputs];
 
+        for(int i = 0;i<features;i++){
+            inputNeurons[i] = neuralNetwork.createInputNeuron();
+        }
+        for(int i = 0;i<outputs;i++){
+            outputNeurons[i] = neuralNetwork.createWorkingNeuron();
+        }
         neuralNetwork.createHiddenNeurons1(100);
         neuralNetwork.setActivationFunctionHidden1(ActivationFunction.ActivationIdentity);
         neuralNetwork.createHiddenNeurons2(10);
         neuralNetwork.setActivationFunctionHidden2(ActivationFunction.ActivationHyperbolicTanget);
 
-        WorkingNeuron out1 = neuralNetwork.createWorkingNeuron();
-//        WorkingNeuron out2 = neuralNetwork.createWorkingNeuron();
-//        WorkingNeuron out3 = neuralNetwork.createWorkingNeuron();
         neuralNetwork.setActivationFunctionOutput(ActivationFunction.ActivationSigmoid);
 
         neuralNetwork.createBias();
         neuralNetwork.createFullConnections(true);
-        double[][] inputs = Einlesen.einlesen("src/Einlesen/wetter.txt",new int[]{0,1},new String[]{"0","1"},2);
         int fehler;
         int epochen = 20000;
         for(int i = 0; i<epochen; i++) {
             fehler = 0;
             for (double[] input: inputs) {
-                in1.setValue(input[0]);
-                in2.setValue(input[1]);
-//                in3.setValue(input[2]);
-//                in4.setValue(input[3]);
-
-//                boolean fehlerCalculated = !(out1.getValue() == input[4] && out2.getValue() == input[5] && out3.getValue() == input[6]);
-                int out = (int) activationFunction.activation(out1.getValue()-0.5);
-                boolean fehlerCalculated = out == input[2];
-                if(!fehlerCalculated) {
+                for(int in = 0;in<inputNeurons.length;in++){
+                    inputNeurons[in].setValue(input[in]);
+                }
+                double[] clazzes = getClazzes(input);
+                if(isFalse(outputNeurons,clazzes)) {
                     fehler++;
-
-                    neuralNetwork.backpropagation(new double[]{input[2]}, learnrate);
+                    neuralNetwork.backpropagation(clazzes, learnrate);
                 }
                 neuralNetwork.reset();
             }
-            if(i%100 == 0)
-            System.out.printf("In der Epoche %d wurde zu %d%% richtig geraten. | (%d/%d)\n",i,(int) (100 - (fehler*100.0)/inputs.length),inputs.length-fehler,inputs.length);
+            if(i%100 == 0) System.out.printf("In der Epoche %d wurde zu %d%% richtig geraten. | (%d/%d)\n",i,(int) (100 - (fehler*100.0)/inputs.length),inputs.length-fehler,inputs.length);
             if(fehler == 0)break;
         }
-        inputs = Einlesen.einlesen("src/Einlesen/wetter2.txt",new int[]{0,1},new String[]{"0","1"},2);
+        inputs = Einlesen.einlesen("src/Einlesen/Iris.txt",features,new String[]{"1","2","3"},clazzId);
         fehler = 0;
         for (double[] input: inputs) {
-            in1.setValue(input[0]);
-            in2.setValue(input[1]);
-//            in3.setValue(input[2]);
-//            in4.setValue(input[3]);
-
-//            boolean fehlerCalculated = !(out1.getValue() == input[4] && out2.getValue() == input[5] && out3.getValue() == input[6]);
-            int out = (int) activationFunction.activation(out1.getValue()-0.5);
-            boolean fehlerCalculated = out != input[2];
-
-            if(fehlerCalculated)
+            for(int in = 0;in<inputNeurons.length;in++){
+                inputNeurons[in].setValue(input[in]);
+            }
+            double[] clazzes = getClazzes(input);
+            if(isFalse(outputNeurons,clazzes))
                 fehler++;
             neuralNetwork.reset();
         }
         System.out.printf("Evaluation anhand von %d Mustern: ",inputs.length);
         System.out.println(((double)(inputs.length-fehler)/(double) inputs.length)*100  + "%");
+    }
+
+    private static double[] getClazzes(double[] input) {
+        double[] r = new double[input.length-clazzId];
+        System.arraycopy(input,clazzId,r,0,r.length);
+        return r;
+    }
+
+
+    private static boolean isFalse(WorkingNeuron[] outputNeurons, double[] clazzes) {
+        for(int i = 0;i<outputNeurons.length;i++){
+            int out = getOutput(outputNeurons[i]);
+            if(out != clazzes[i]) return true;
+        }
+        return false;
+
+    }
+
+    public static int getOutput(WorkingNeuron out){
+        return (int) activationFunction.activation(out.getValue()-0.5);
     }
 
 }
